9c1fdc41cba794377e2880dc826a5e12b1b3ef6e
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-07-17 18:56:40 +0300

    +

--- ThreadsNew/main.swift
+++ ThreadsNew/main.swift
@@ -1,12 +1,28 @@
-//
-//  main.swift
-//  ThreadsNew
-//
-//  Created by Артем on 7/16/19.
-//  Copyright © 2019 Артем. All rights reserved.
-//
-
 import Foundation
+func isSimple(_ num: Int) -> Bool {
+    if num < 0 {
+        return false
+    }
+    if Range(0...3).contains(num) {
+        return true
+    }
+    for i in 2...num / 2 {
+        if num % i == 0 {
+            return false
+        }
+    }
+    return true
+}
 
-print("Hello, World!")
+
+for j in 0..<10 {
+    var count = 0
+    for i in 1000 * j..<1000 * j + 1000 {
+        if isSimple(i) {
+            count += 1
+        }
+    }
+    print(1000 * j, " - ", 1000 * j + 1000, count)
+}
+
 
